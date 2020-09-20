4089cf473dd969bd40b1610223fe8811cd2b9269
airus2302 <airus2302@gmail.com>
2020-09-20 22:10:48 +0900

    local db crud

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,13 @@
-const airus = {
-  name: 'Airus',
-  age: 18,
-  gender: 'male',
-};
+import { getMovies, getById, addMovie, deleteMovie } from './db';
 
 const resolvers = {
   Query: {
-    person: () => airus,
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
